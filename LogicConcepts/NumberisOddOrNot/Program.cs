d7ef10c4b11609f1d26346c39061c788ff164b01
MARIA TERESA GUERRA ALVAREZ <mariaguerra145494@correo.itm.edu.co>
2026-08-08 21:02:50 -0500

    Exercise 01 finish

---
+++ LogicConcepts/NumberisOddOrNot/Program.cs
@@ -0,0 +1,27 @@
+﻿do
+{
+    Console.Write("Ingrese número o Ctrol C para terminar:");
+    var numberString = Console.ReadLine();
+    try
+    {
+        var numberInt = int.Parse(numberString!);
+        if (numberInt % 2 == 0)
+        {
+            Console.WriteLine($"El número: {numberInt}, es par.");
+
+        }
+        else
+        {
+            Console.WriteLine($"El número_ {numberInt}, es impar.");
+        }
+    }
+    catch 
+    {
+
+        Console.WriteLine($"El número ingresado: {numberString}, no es valido. Solo utilice carácteres numéricos.");
+        
+    }
+  
+} while (true);
+
+
