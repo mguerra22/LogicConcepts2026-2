7f380214d430c675036012f4509b05f27038ab5f
MARIA TERESA GUERRA ALVAREZ <mariaguerra145494@correo.itm.edu.co>
2026-08-13 17:32:59 -0500

    Improve exit program

---
+++ LogicConcepts/IsLeapYear/Program.cs
@@ -0,0 +1,35 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    var currentYear = DateTime.Now.Year;
+    var message = string.Empty;
+    var year = ConsoleExtension.GetInt("Ingrese año: ");
+
+    if (year == currentYear)
+    {
+        message = "es";
+    }
+    else if (year > currentYear)
+    {
+        message = "va a ser";
+    }
+    else
+    {
+        message = "fue";
+    }
+
+    var isLeapYear = DateUtilities.IsLeapYear(year);
+    Console.WriteLine($"El año: {year}, {(isLeapYear ? "SI" : "NO")} {message} biciesto.");
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]í, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+Console.WriteLine("Game Over.");
--- LogicConcepts/Shared/ConsoleExtension.cs
+++ LogicConcepts/Shared/ConsoleExtension.cs
@@ -24,4 +24,21 @@ public static class ConsoleExtension
         }
         return null;
     }
+
+    public static bool IsLeapYear(int year)
+    {
+        if (year % 4 == 0)
+        {
+            if (year % 100 == 0)
+            {
+                if (year % 400 == 0)
+                {
+                    return true;
+                }
+                return false;
+            }
+            return true;
+        }
+        return false;
+    }
 }
