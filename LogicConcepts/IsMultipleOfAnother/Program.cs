eae404b685a477006e6a14e3517abae42f04294b
MARIA TERESA GUERRA ALVAREZ <mariaguerra145494@correo.itm.edu.co>
2026-08-13 16:54:12 -0500

    Is multiple of another

---
+++ LogicConcepts/IsMultipleOfAnother/Program.cs
@@ -0,0 +1,26 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    var a = ConsoleExtension.GetInt("Ingrese primer número : ");
+    var b = ConsoleExtension.GetInt("Ingrese segundo número: ");
+    if (b % a == 0)
+    {
+        Console.WriteLine($"{a} es múltiplo {b}");
+    }
+    else
+    {
+        Console.WriteLine($"{a} no es múltiplo {b}");
+    }
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]í, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+Console.WriteLine("Game Over.");
