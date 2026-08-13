45bc065bcf2c6608056ab686ebafb523247e1a60
MARIA TERESA GUERRA ALVAREZ <mariaguerra145494@correo.itm.edu.co>
2026-08-13 15:22:35 -0500

    Exercise 02 complete

---
+++ LogicConcepts/TheGreathestNumber/Program.cs
@@ -0,0 +1,41 @@
+﻿var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    Console.WriteLine("Ingrese tres números diferentes");
+
+    Console.Write("Ingrese primer número: ");
+    var a = int.Parse(Console.ReadLine());
+
+    Console.Write("Ingrese segundo número: ");
+    var b = int.Parse(Console.ReadLine());
+
+    Console.Write("Ingrese tercer número: ");
+    var c = int.Parse(Console.ReadLine());
+
+    if (a > b && a > c)
+    {
+        Console.WriteLine($"El número mayor es: {a}");
+    }
+    else if (b > a && b > c)
+    {
+        Console.WriteLine($"El número mayor es: {b}");
+    }
+    else
+    {
+        Console.WriteLine($"El número mayor es: {c}");
+    }
+
+    do
+    {
+      
+        Console.Write("¿Deseas continuar [S]i, [N]o?: ");
+        answer = Console.ReadLine();
+    }
+
+    while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+}
+while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+Console.WriteLine("Game Over.");
